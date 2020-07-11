f1db0364afd97ef368d2cba44dca671fa678d281
mohit <mohit.dhiman.sarkaghat@gmail.com>
2020-07-11 18:53:18 +0530

    Add to update posts

--- app.js
+++ app.js
@@ -85,3 +85,14 @@ app.get('/getpost/:id', (req, res) => {
   });
 });
 
+// Update post
+app.get('/updatepost/:id', (req, res) => {
+  let newTitle = 'Updated Title';
+  let sql = `UPDATE posts SET title = '${newTitle}' WHERE id = ${req.params.id}`;
+  let query = db.query(sql, (err, result) => {
+      if(err) throw err;
+      console.log(result);
+      res.send('Post updated...');
+  });
+});
+
