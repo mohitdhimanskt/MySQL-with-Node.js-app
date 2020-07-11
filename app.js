1c8f5f1f1e60fa189151830040226887bf95b7ae
mohit <mohit.dhiman.sarkaghat@gmail.com>
2020-07-11 18:44:32 +0530

    Add to create connection

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mysql = require('mysql');
+
+// Create connection
+const db = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : '123456',
+    database : 'nodemysql'
+});
+
+// Connect
+db.connect((err) => {
+    if(err){
+        throw err;
+    }
+    console.log('MySql Connected...');
+});
+
+const app = express();
