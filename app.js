f1c427d75251be76ee33a9332b5bb0d9ef17af42
mohit <mohit.dhiman.sarkaghat@gmail.com>
2020-07-11 18:46:41 +0530

    Add to create table

--- app.js
+++ app.js
@@ -29,3 +29,14 @@ app.get('/createdb', (req, res) => {
       res.send('Database created...');
   });
 });
+
+
+// Create table
+app.get('/createpoststable', (req, res) => {
+  let sql = 'CREATE TABLE posts(id int AUTO_INCREMENT, title VARCHAR(255), body VARCHAR(255), PRIMARY KEY(id))';
+  db.query(sql, (err, result) => {
+      if(err) throw err;
+      console.log(result);
+      res.send('Posts table created...');
+  });
+});
